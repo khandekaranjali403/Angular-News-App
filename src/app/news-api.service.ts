da5f8bd1e8b853d5e6168585f388a56c60233d42
Anjali Khandekar <khandekaranjali403@gmail.com>
2023-09-11 08:30:43 +0530

    Angular new app

---
+++ src/app/news-api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  
+  api_key ='a9c63f39ed274a8f9134539f967547ee';
+  
+
+  constructor(private http:HttpClient) { }
+
+
+  initSources(){
+	 return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+
+
+  initArticles(){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+
+  getArticlesByID(source: String){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+
+
+
+}
